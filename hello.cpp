bd3a5c7886dbb1cb2468db20d3ad7eaf37320e0c
Jake Perez <jacobanthony.perez@gmail.com>
2018-03-12 20:48:22 -0700

    Refactored and modularized

--- hello.cpp
+++ hello.cpp
@@ -3,12 +3,18 @@
 
 using namespace std;
 
+string helloPerson(string person) {
+  return "Hello " + person;
+}
+
 int main() {
 
   string yourName;
   cout << "What is your name?" << endl;
   cin >> yourName;
-  cout << "Hello " + yourName << endl;
+  // cout << "Hello " + yourName << endl;
   // cout << "Hello World!" << endl;
+  string outgoingMessage = helloPerson(yourName);
+  cout << outgoingMessage << endl;
   return 0;
 }
